f5cde086ccfcad9d6c542d6920dc88b127bb9280
your_username <rrulyabdul@gmail.com>
2023-03-04 06:44:13 +0700

    coba

--- hw9/index.js
+++ hw9/index.js
@@ -4,7 +4,12 @@ var pool = require("./queries.js");
 
 var routes = require("./routes.js");
 
-app.use("/", routes);
+// Menerima request body Json
+app.use(express.json());
+// Menerima request body urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
 
 pool.connect((err, res) => {
   console.log(err);
